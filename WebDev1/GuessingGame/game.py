ad7e8aee3b27ba20ddaf3368ee4f5929df40577d
Kevin Andersen <c00225541@itcarlow.ie>
2019-11-06 16:46:39 +0000

    Guessing complete and storing score and num of guesses

--- WebDev1/GuessingGame/game.py
+++ WebDev1/GuessingGame/game.py
@@ -2,38 +2,81 @@ from flask import Flask, render_template, session, request
 import random
 import pickle
 import os
+import datetime
 
+LEADERBOARD = "data/leaderboard.pickle"
 app = Flask(__name__)
-FNAME = "data/randNumber.pickle"
-randomNumber = random.randint(0, 1000)
 @app.route("/guessingGame")
-def game():
-    return render_template("game.html", 
-    the_title="Guess the Number")
+def guessNumber():
+    randNum = random.randint(0,1000)
+    session["number"] = randNum
+    session["guess"] = 0
+    return render_template(
+        "game.html",
+        the_title = "Guess the Number",
+        the_number = randNum,
+        the_script = "",
+        the_num = randNum
+    )
+@app.route("/guessedNumber", methods =["POST"])
+def guessedNumber():
+    randNum = session["number"]
+    if request.form["guessedNumber"] == "":
+        return render_template( 
+        "game.html",
+        the_title = "Guess the Number",
+        the_number = randNum,
+        the_script = "Input a number",
+        )
+    else:
+        numberGuessed = (int)(request.form["guessedNumber"])
 
-@app.route("/response", methods = ["POST"])
-def response():
-    guess_number = request.form["number"]
-    response = ""
-    if(guess_number == randomNumber):
-        response = "Congratulations, you guessed correctly!"
-    elif(guess_number < randomNumber):
-           response = "Sorry, your guess is too low"
-    else:
-        response = "Sorry, your guess is too high"
-@app.route("/storeGuessedNum", methods=["POST"])
-def store_number():
-    guessed_num = request.form["number"]
-    #if file doesnt exist-create a new empty pickle file
-    #if the file does exist-grab the current data stored in the pickle.
-    if not os.path.exists(FNAME):
+        session["guess"] = session["guess"] + 1
+        noOfGuesses = session["guess"]
+    if numberGuessed > randNum:
+        return render_template( 
+        "game.html",
+        the_title = "Guess the Number",
+        the_number = randNum,
+        the_script = "Your guess is too high"
+        )
+    elif numberGuessed < randNum:
+        return render_template( 
+        "game.html",
+        the_title = "Guess the Number",
+        the_number = randNum,
+        the_script = "Your guess is too low"
+        )
+    else :
+        return render_template( 
+        "score.html",
+        the_title = "Guess the Number",
+        the_number = randNum,
+        the_guesses = noOfGuesses
+        )
+@app.route("/recordName", methods =["POST"])
+def storeScore():
+    player_name = request.form["player_name"]
+    player_guess = session["guess"]
+    if not os.path.exists(LEADERBOARD):
         data = []
-    else:
-        with open(FNAME, "rb") as pf:
+    else: 
+        with open(LEADERBOARD,"rb") as pf:
             data = pickle.load(pf)
-            data.append(randomNumber,guessed_num)
-    with open(FNAME, "wb") as pf:
-         pickle.dump(data, pf)
-    return 
-app.secret_key = "flkgdn kldndfngfjdngrnfjngfjgb yrbbebisbfidbb "
+    data.append((player_guess,player_name))
+    with open(LEADERBOARD,"wb") as pf:
+            pickle.dump(data,pf)
+    return "Your name and score is now recorded"
+@app.route("/highscore")
+def show_highscores():
+       with open(LEADERBOARD,"rb") as pf:
+            data = pickle.load(pf)
+       return render_template(
+            "leaderboard.html",
+            the_title = "Highscores",
+            the_data = data
+        )
+    
+app.secret_key = "Kevin123 ggwp"
+
 app.run(debug=True)
