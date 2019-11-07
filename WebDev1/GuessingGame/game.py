a9758dbee02dc5e369a247089283c547d0be1243
Kevin Andersen <c00225541@itcarlow.ie>
2019-11-07 14:33:06 +0000

    Everything except sorting table

--- WebDev1/GuessingGame/game.py
+++ WebDev1/GuessingGame/game.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, request
 import random
 import pickle
 import os
-import datetime
+import time
 
 LEADERBOARD = "data/leaderboard.pickle"
 app = Flask(__name__)
@@ -11,6 +11,7 @@ def guessNumber():
     randNum = random.randint(0,1000)
     session["number"] = randNum
     session["guess"] = 0
+    session["start"] = time.perf_counter()
     return render_template(
         "game.html",
         the_title = "Guess the Number",
@@ -48,22 +49,27 @@ def guessedNumber():
         the_script = "Your guess is too low"
         )
     else :
+        session["end"] = time.perf_counter()
+        time_taken = round(session["end"] - session["start"],2)
+        session["time"] = time_taken
         return render_template( 
         "score.html",
         the_title = "Guess the Number",
         the_number = randNum,
-        the_guesses = noOfGuesses
+        the_guesses = noOfGuesses,
+        the_time = time_taken
         )
 @app.route("/recordName", methods =["POST"])
 def storeScore():
     player_name = request.form["player_name"]
     player_guess = session["guess"]
+    player_time = session["time"]
     if not os.path.exists(LEADERBOARD):
         data = []
     else: 
         with open(LEADERBOARD,"rb") as pf:
             data = pickle.load(pf)
-    data.append((player_guess,player_name))
+    data.append((player_guess,player_name,player_time))
     with open(LEADERBOARD,"wb") as pf:
             pickle.dump(data,pf)
     return "Your name and score is now recorded"
