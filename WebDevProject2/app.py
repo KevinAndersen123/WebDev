8d93ef5e705f524c93f7f39e9520da32caee39f2
Kevin Andersen <c00225541@itcarlow.ie>
2020-02-12 21:53:01 +0000

    Screens added

--- WebDevProject2/app.py
+++ WebDevProject2/app.py
@@ -26,17 +26,30 @@ def titles():
         data = cursor.fetchall()
         return render_template("viewTitles.html", the_data = data)
 
-@app.route("/gameComment")
-def writeComment():
+@app.route("/writeReview")
+def writeReview():
     return render_template("writeReview.html")
 
 @app.route("/submitReview", methods=["POST"])
 def submitReview():
     with DBcm.UseDatabase(config) as cursor:
-        SQL = ""
+        SQL = "update titles set likes = (select count(*) from comment where comment.title_id = 1 && comment.like_dislike = 1) where id =" + session["gameid"]
+        cursor.execute(SQL)
+
+    with DBcm.UseDatabase(config) as cursor:
+        SQL = "update titles set dislikes = (select count(*) from comment where comment.title_id = 1 && comment.like_dislike = 2 ) where id =" + session["gameid"]
+        cursor.execute(SQL)
+
+    with DBcm.UseDatabase(config) as cursor:
+        SQL = "update titles set no_comments = (select count(*) from comment where comment.title_id = " + session["gameid"]+ ") where id =" + session["gameid"]
+        cursor.execute(SQL)
+
+    with DBcm.UseDatabase(config) as cursor:
+        SQL = """select titles.id, titles.name , titles.likes , titles.dislikes , titles.no_comments , titles.release_year , genre.name , titles.game_studio , platform.name from titles,genre,platform  
+                 where titles.genre = genre.id and titles.platform = platform.id"""
         cursor.execute(SQL)
         data = cursor.fetchall()
-        render_template("viewTitles.html", the_data = data)
+        return render_template("viewTItles.html", the_data = data)
 
 @app.route("/gameDetails", methods=["POST"])
 def gameDetails():
@@ -47,6 +60,23 @@ def gameDetails():
         data = cursor.fetchall()
         return render_template("gameDetails.html", the_data = data)
 
+@app.route("/insertGamePage")
+def insertGamePage():
+     with DBcm.UseDatabase(config) as cursor:
+        SQL = "select * from genre"
+        cursor.execute(SQL)
+        data = cursor.fetchall()
+        return render_template("newGame.html", the_data = data)
+
+@app.route("/insertNewGame" , methods=["POST"])
+def insertNewGame():
+     with DBcm.UseDatabase(config) as cursor:
+        SQL = """select titles.id, titles.name , titles.likes , titles.dislikes , titles.no_comments , titles.release_year , genre.name , titles.game_studio , platform.name from titles,genre,platform  
+                 where titles.genre = genre.id and titles.platform = platform.id"""
+        cursor.execute(SQL)
+        data = cursor.fetchall()
+        return render_template("viewTitles.html", the_data = data)
+    
 app.secret_key = "Kevin Andersen123 fffffff ggggggg bbbbb"
 
 if __name__ == "__main__":
